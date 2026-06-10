f1b95df7ce16b445404ce75da01b17c0f05e493f
Bbrian223 <barreragomezb2800@gmail.com>
2026-06-09 22:14:56 -0300

    Agregar archivos de proyecto.

---
+++ Ad.Works.Domain/Entities/UnitMeasure.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ad.Works.Domain.Entities;
+
+/// <summary>
+/// Unit of measure lookup table.
+/// </summary>
+public partial class UnitMeasure
+{
+    /// <summary>
+    /// Primary key.
+    /// </summary>
+    public string UnitMeasureCode { get; set; } = null!;
+
+    /// <summary>
+    /// Unit of measure description.
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Date and time the record was last updated.
+    /// </summary>
+    public DateTime ModifiedDate { get; set; }
+
+    public virtual ICollection<Product> ProductSizeUnitMeasureCodeNavigations { get; set; } = new List<Product>();
+
+    public virtual ICollection<Product> ProductWeightUnitMeasureCodeNavigations { get; set; } = new List<Product>();
+}
