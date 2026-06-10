921005d339afc270543de1306f1007921a6c2427
Bbrian223 <barreragomezb2800@gmail.com>
2026-06-10 20:53:47 -0300

    Add basic product repository

---
+++ Ad.Works.Infrastructure/Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ad.Works.Domain.Entities;
+
+namespace Ad.Works.Infrastructure.Interfaces
+{
+    public interface IProductRepository
+    {
+        public Task<IEnumerable<Product>> GetAllAsync();
+        public Task<Product> GetAsync(int id);
+    }
+}
---
+++ Ad.Works.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,46 @@
+﻿using Ad.Works.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ad.Works.Domain.Entities;
+using Ad.Works.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ad.Works.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AdventureWorksDbContext _context;
+
+        public ProductRepository(AdventureWorksDbContext context)
+        {
+            _context = context;
+        }
+
+        #region GET ALL
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            var list = await _context.Products.
+                ToListAsync();
+
+            if (!list.Any())
+                return null;
+
+            return list;
+        }
+        #endregion
+
+        #region GET BY ID
+        public async Task<Product> GetAsync(int id)
+        {
+            var product = await _context.Products
+                .Where(u => u.ProductId == id)
+                .SingleOrDefaultAsync();
+
+            return product;
+        }
+        #endregion
+    }
+}
